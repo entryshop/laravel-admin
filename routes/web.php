7a3d41ae8468697ddb7850436a820ca130aaf081
Alex <alex@parse.cn>
2025-01-15 21:36:54 +0800

    Delete migrations and model, update web routes.<commit message>

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,8 @@
 
 use Entryshop\Admin\Http\Controllers\AuthController;
 
-Route::group(['middleware' => 'web'], function () {
+admin()->group(function () {
     Route::get('login', [AuthController::class, 'login'])->name('login');
     Route::post('login', [AuthController::class, 'submitLogin'])->name('admin.login.submit');
     Route::any('logout', [AuthController::class, 'logout'])->name('admin.logout');
 });
-
